fcb96c487d9db41c243f07ed9fd61758cdbf70b3
Brigitte Hofmeister <bhofmei@gmail.com>
2017-08-16 14:28:05 -0400

    basic package files

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
 define([
-           'dojo/_base/declare',
-           'JBrowse/Plugin'
-       ],
-       function(
-           declare,
-           JBrowsePlugin
-       ) {
-return declare( JBrowsePlugin,
-{
-    constructor: function( args ) {
+    'dojo/_base/declare',
+    'JBrowse/Plugin'
+  ],
+  function (
+    declare,
+    JBrowsePlugin
+  ) {
+    return declare(JBrowsePlugin, {
+      constructor: function (args) {
         var browser = args.browser;
 
         // do anything you need to initialize your plugin here
-        console.log( "WiggleSVGPlotPlugin plugin starting" );
+        console.log("WiggleSVGPlotPlugin plugin starting");
+        this.config.version = 'v0.0.1';
 
-    }
-});
-});
+      }
+    });
+  });
